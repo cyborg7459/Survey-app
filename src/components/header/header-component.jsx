7dbb877ac8d39c1272f04cc82a9abb0660720cec
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-03-27 18:35:29 +0530

    implement sign out feature

--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -2,6 +2,8 @@ import './header-style.scss';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import {logUserOut} from '../../redux/users/user-actions';
+
 const Header = (props) => {
     const users = props.users;
     return (
@@ -11,7 +13,10 @@ const Header = (props) => {
                     props.history.push('/');
                 }} style={{cursor : 'pointer'}} className='size20'>What's your opinion ?</span>
                 {
-                    users.currentUser ? <span> {users.currentUser.name} </span> : null
+                    users.currentUser ? 
+                    <span className='size13'> Signed in as <strong>{users.currentUser.name.split(' ')[0]}</strong> &nbsp; &nbsp; <span onClick={() => props.logOut()} style={{cursor: 'pointer'}}>Sign out</span> </span> 
+                    : 
+                    <span className='size13' style={{cursor : 'pointer'}} onClick={() => {props.history.push('/login')}}>Login</span>
                 }
             </div>
         </div>
@@ -22,4 +27,8 @@ const mapStateToProps = state => ({
     users : state.users
 });
 
-export default withRouter(connect(mapStateToProps)(Header));
+const mapDispatchToProps = dispatch => ({
+    logOut : () => dispatch(logUserOut())
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));
