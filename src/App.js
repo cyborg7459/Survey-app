b1475e4f1085f0e1864f7e1e91fd9459c191dbff
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-03-27 11:43:38 +0530

    initial setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './font-sizes.css';
 
+import Header from './components/header/header-component';
+
 class App extends React.Component {
   render() {
     return (
       <div className='app'>
-        <h1>Hello World</h1>
+        <Header />
       </div>
     )
   }
---
+++ src/components/header/header-component.jsx
@@ -0,0 +1,13 @@
+import './header-style.scss';
+
+const Header = () => {
+    return (
+        <div id="header-container">
+            <div id="header">
+                <h1 className='size20 mt-0'>What's your opinion ?</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
