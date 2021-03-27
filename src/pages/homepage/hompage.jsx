6c4b0ce9fab5467f6337182ecb90dab8e5f5aa57
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-03-27 12:17:56 +0530

    complete homepage

--- src/pages/homepage/hompage.jsx
+++ src/pages/homepage/hompage.jsx
@@ -1,11 +1,24 @@
 import './homepage-style.scss';
+import { Row, Col } from 'react-bootstrap';
 
 import coverimg from '../../gallery/main.svg';
 
 const Homepage = () => {
     return (
         <div id="homepage-container">
-
+            <Row id="homepage-inner">
+                <Col xl={7}>            
+                    <img id="coverImg" src={coverimg} alt=""/>
+                </Col>
+                <Col className='text-col' xl={5}>
+                    <h1 className='main-heading size32'>Share your opinion with the world</h1>
+                    <br/>
+                    <p className='size16'>Participate in surveys ranging a variety of topics from sports to politics to education to science. Compare your views with the general ones. 
+                    <br/> <br/> Or create your own survey and share among your friends
+                    </p>
+                    <div className="btn btn-lg">Get started</div>
+                </Col>
+            </Row>
         </div>
     )
 }
