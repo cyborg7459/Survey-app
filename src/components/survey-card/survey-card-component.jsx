9f70520e3db80b055f83db3fe78942323e2acb7b
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-03-29 01:27:59 +0530

    set up the surveys page

---
+++ src/components/survey-card/survey-card-component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './survey-card-styles.scss';
+
+class SurveyCard extends React.Component {
+    render() {
+        return (
+            <div className="survey-card">
+                <div className="main-heading">Hello Card</div>
+            </div>
+        )
+    }
+}
+
+export default SurveyCard;
--- src/pages/mainpage/main.jsx
+++ src/pages/mainpage/main.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+import './main-styles.scss';
+import { firestore } from '../../firebase/firebase.utils';
+import SurveyCard from '../../components/survey-card/survey-card-component';
 
 class Main extends React.Component {
+
+    componentDidMount() {
+        this.getSurveys();
+    }
+
+    getSurveys = async () => {
+        const surveyRef = firestore.collection('surveys');
+        const surveySanpshot = await surveyRef.get();
+        return surveySanpshot.docs;
+    }
+
     render() {
         return (
             <div className="main-page-container">
-                <h1>Hello World</h1>
-                <div className="surveys-container"></div>
+                <div className="main-page-inner">
+                    <h1 className='main-heading'>Active surveys</h1>
+                    <Row className="surveys-container">
+                        {
+                            this.getSurveys().map(survey => {
+                                return (
+                                    <SurveyCard />
+                                )
+                            })
+                        }
+                    </Row>
+                </div>
             </div>
         )
     }
