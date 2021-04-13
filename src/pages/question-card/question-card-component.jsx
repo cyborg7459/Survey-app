50cb95b6f13b2b664f5f540f0418d5a34ea63cdb
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-04-14 04:04:43 +0530

    implement the question card

--- src/pages/question-card/question-card-component.jsx
+++ src/pages/question-card/question-card-component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './question-card-style.csss';
+import './question-card-style.scss';
 
 class QuestionCard extends React.Component {
     render() {
         return (
             <div className="question-card">
-                <h1>{this.props.question.title}</h1>
+                <h4 className='m-0'>{this.props.question.title}</h4>
             </div>
         )
     }
