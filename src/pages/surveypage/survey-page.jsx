1cdab54dd133eb4f34515693ebc0e988ea43dda6
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-04-14 04:04:30 +0530

    implement build question array function

--- src/pages/surveypage/survey-page.jsx
+++ src/pages/surveypage/survey-page.jsx
@@ -4,13 +4,14 @@ import { withRouter } from 'react-router-dom';
 import './survey-styles.scss';
 import Loader from '../../components/loader/loader.component';
 import { firestore } from '../../firebase/firebase.utils';
+import QuestionCard from '../question-card/question-card-component';
 
 class SurveyPage extends React.Component {
 
     state = {
         isLoading : true,
-        surveyDetails : {},
-        surveyQuestions : []
+        loaderText : 'Preparing the survey',
+        surveyDetails : {}
     }
 
     componentDidMount() {
@@ -25,8 +26,22 @@ class SurveyPage extends React.Component {
         const surveySnapshot = await surveyRef.get();
         const questionsRef = surveyRef.collection('questions');
         const questionsSnapshot = await questionsRef.get();
-        let questions = [];
-        questionsSnapshot.docs.map(async question => {
+        this.setState({
+            surveyDetails : surveySnapshot.data(),
+            loaderText : 'Fetching questions'
+        })
+        const questions = this.buildQuestionsArray(questionsRef , questionsSnapshot.docs);
+        setTimeout(() => {
+            this.setState({
+                surveyQuestions : questions,
+                isLoading : false
+            })
+        }, 1000)
+    }
+
+    buildQuestionsArray = (questionsRef, questions) => {
+        let questionsArray = [];
+        questions.map(async question => {
             let curQuestion = question.data();
             curQuestion.id = question.id;
             const optionsRef = questionsRef.doc(question.id).collection('options');
@@ -38,26 +53,38 @@ class SurveyPage extends React.Component {
                 options.push(optionDetails);
             })
             curQuestion.options = options;
-            questions.push(curQuestion);
+            questionsArray.push(curQuestion);
         })
-        this.setState({
-            surveyDetails : surveySnapshot.data(),
-            surveyQuestions : questions,
-            isLoading : false
-        })
+        return questionsArray;
     }
 
     render() {
-        return (
-            <div className="survey-page-container">
-                {
-                    this.state.isLoading ? <Loader text = 'Preparing the survey' /> : null
-                }
-                <div className="survey-page-inner">
-                    <div className="size28 d-flex align-items-center main-heading">{this.state.surveyDetails.title} | <span className='ml-4 size13'>by {this.state.surveyDetails.byUser}</span></div>
+        if(this.state.isLoading) {
+            return (
+                <div className="survey-page-container">
+                    {
+                        this.state.isLoading ? <Loader text = {this.state.loaderText} /> : null
+                    }
                 </div>
-            </div>
-        )
+            )
+        }
+        else {
+            return (
+                <div className="survey-page-container">
+                    <div className="survey-page-inner">
+                        <div className="size28 d-flex align-items-center main-heading">{this.state.surveyDetails.title} | <span className='ml-4 size13'>by {this.state.surveyDetails.byUser}</span></div>
+                        {
+                            this.state.surveyQuestions.map(surveyQuestion => {
+                                return (
+                                    <QuestionCard key = {surveyQuestion.id} question = {surveyQuestion}></QuestionCard>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            )
+        }
+        
     }
 }
 
