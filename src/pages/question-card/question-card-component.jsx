b40d89239fa38b0d01dc7e2e33d1bfc8b6d651d3
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-04-14 04:20:22 +0530

    complete UI for question card component

--- src/pages/question-card/question-card-component.jsx
+++ src/pages/question-card/question-card-component.jsx
@@ -6,7 +6,16 @@ class QuestionCard extends React.Component {
     render() {
         return (
             <div className="question-card">
-                <h4 className='m-0'>{this.props.question.title}</h4>
+                <h3 className='text-center m-0 mb-3'>{this.props.question.title}</h3>
+                {
+                    this.props.question.options.map(option => {
+                        return (
+                            <div className="size13 btn option" key={option.id} id = {option.id}>
+                                {option.optionVal}
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
