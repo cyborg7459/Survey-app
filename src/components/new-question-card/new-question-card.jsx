fe7d09c01d23ae8268131ba5433f2c18a8b2c876
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-04-17 22:22:51 +0530

    complete addQuestion() function in new question card component

--- src/components/new-question-card/new-question-card.jsx
+++ src/components/new-question-card/new-question-card.jsx
@@ -5,7 +5,7 @@ import './new-question-card-styles.scss';
 class NewQuestionCard extends React.Component {
 
     state = {
-        optCount : 1
+        optCount : 2
     }
 
     
@@ -16,24 +16,47 @@ class NewQuestionCard extends React.Component {
         })
     }
 
+    addQuestion = () => { 
+        const qCard = document.getElementById(this.props.cardID);
+        const questionTitle = qCard.firstChild.value.trim();
+        if(questionTitle === "") 
+            return alert("A question must have a title");
+        const options = document.querySelectorAll(`#${this.props.cardID} .option`);
+        let optionsArray = [];
+        options.forEach(option => {
+            const optVal = option.value.trim();
+            if(optVal) optionsArray.push(optVal);
+        })
+        if(optionsArray.length < 2)
+            return alert("A question must have at least 2 valid options");
+        const question = {
+            title : questionTitle,
+            options : optionsArray
+        }
+        console.log(question);
+    }
+
     render() {
 
         let optionsGroup = [];
         for(let i = 1; i <= this.state.optCount; i++) {
             optionsGroup.push(
-                <input className='input' placeholder={`Enter option ${i}`}/>
+                <input className='input option' placeholder={`Enter option ${i}`}/>
             )
         }
 
         return (
-            <div className="new-question-card">
+            <div id={this.props.cardID} className="new-question-card">
                 <input className='input input-borderless size19' type="text" placeholder="Type your question here"/>
                 <div className='d-flex align-items-center px-3'>
                     <label className='pt-2 mr-4 size12'>Select number of options : </label>
-                    <input min="1" id='opt-count' onChange={this.setOptCount} style={{width : "70px"}} type="number" className='input input-borderless' placeholder="Select number of options" defaultValue="1"/>
+                    <input min="2" id='opt-count' onChange={this.setOptCount} style={{width : "70px"}} type="number" className='input input-borderless' placeholder="Select number of options" defaultValue="2"/>
                 </div>
                 <div className="options-group">
                     {optionsGroup}
+                </div>
+                <div onClick={this.addQuestion} className='d-flex justify-content-end'>
+                    <button style={{width : "200px"}} className='mr-5 mt-5 mb-3 btn'>Add question</button>
                 </div>
             </div>
         )
--- src/pages/newsurveypage/new-survey-page.jsx
+++ src/pages/newsurveypage/new-survey-page.jsx
@@ -14,12 +14,18 @@ class NewSurveyPage extends React.Component {
         })
     };
 
+    increaseQuestionCount = () => {
+        this.setState({
+            questionCount : this.state.questionCount + 1
+        });
+    }
+
     render() {
         
         const questionCards = [];
         for(let i = 0; i < this.state.questionCount; i++) {
             questionCards.push(
-                <NewQuestionCard/>
+                <NewQuestionCard key={i} cardID={`card-${i}`}/>
             )
         }
 
@@ -29,6 +35,8 @@ class NewSurveyPage extends React.Component {
                     <h1 className='main-heading'>Create your own survey</h1>
                     <h1 className='mb-5 size13'>Learn the world's views on topics that matter to you</h1>
                     {questionCards}
+                    <button onClick={this.increaseQuestionCount} className='btn btn-block mt-5'>Add another question</button>
+                    <button className='mb-5 btn btn-block mt-3'>Publish survey</button>
                 </div>
             </div>
         )
