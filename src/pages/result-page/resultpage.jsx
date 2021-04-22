409bfa2bf9c5f3b300852057386797e636f74405
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-04-22 12:43:11 +0530

    initialize the results page and charting library

--- src/pages/result-page/resultpage.jsx
+++ src/pages/result-page/resultpage.jsx
@@ -1,6 +1,50 @@
 import React from 'react';
 
 class ResultPage extends React.Component {
+
+    state = {
+        data: [
+            {
+                question : 'Who is the best player of all times',
+                options : [
+                    {
+                        value : "Messi",
+                        votes : 189
+                    },
+                    {
+                        value : "Ronaldo",
+                        votes : 132
+                    },
+                    {
+                        value : "Maradona",
+                        votes : 109
+                    },
+                    {
+                        value : "Pele",
+                        votes : 175
+                    }
+                ]
+            },
+            {
+                question : "Who is the best striker in the world",
+                options : [
+                    {
+                        value : "Lewandowski",
+                        votes : 555
+                    },
+                    {
+                        value : "Harry Kane",
+                        votes : 653
+                    },
+                    {
+                        value : "Aubameyang",
+                        votes : 122
+                    }
+                ]
+            }
+        ]
+    }
+
     render() {
         return (
             <div className="page-container">
