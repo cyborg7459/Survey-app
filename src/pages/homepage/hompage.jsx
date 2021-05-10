ce0b3e47aca67626145a4f569deeed9ce54500b6
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-11 01:14:04 +0530

    update the homepage

--- src/pages/homepage/hompage.jsx
+++ src/pages/homepage/hompage.jsx
@@ -1,27 +1,32 @@
 import './homepage-style.scss';
 import { withRouter } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
 
-import coverimg from '../../gallery/main.svg';
+import coverimg from '../../gallery/img.svg';
 
 const Homepage = (...props) => {
     return (
         <div id="homepage-container">
-            <Row id="homepage-inner">
-                <Col xl={7}>            
-                    <img id="coverImg" src={coverimg} alt=""/>
-                </Col>
-                <Col className='text-col' xl={5}>
-                    <h1 className='main-heading size32'>Share your opinion with the world</h1>
-                    <br/>
-                    <p className='size16'>Participate in surveys ranging a variety of topics from sports to politics to education to science. Compare your views with the general ones. 
-                    <br/> <br/> Or create your own survey and share among your friends
-                    </p>
-                    <div onClick = {() => {
-                        props[0].history.push('/login');
-                    }} className="btn btn-lg">Get started</div>
-                </Col>
-            </Row>
+            <div className="custom-shape-divider-bottom-1620380295" >
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+                </svg>
+            </div>
+            <div id="homepage-inner">
+                <img src={coverimg} id="main-img" alt="imgs" />       
+                <div id="heading">
+                    <h1 id="back-heading">WHAT'S YOUR</h1>
+                    <h1 id="front-heading">Opinion</h1>
+                </div>
+                <p id="heading-text">
+                    Participate in surveys ranging a variety of topics from sports to politics to education to science. Compare your views with the general ones.
+                    Or create your own survey and share among your friends
+                </p>
+                <button onClick={() => {
+                    props[0].history.push('/login');
+                }} class="btn" id="get-started-btn">
+                    Get started
+                </button>
+            </div>
         </div>
     )
 }
