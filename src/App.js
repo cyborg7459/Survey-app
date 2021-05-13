64703fe6e0b3ae2a3f25094debf078dfe71f9191
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-14 02:31:53 +0530

    remove auth by email and passoword

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import './font-sizes.css';
 import Header from './components/header/header-component';
 import Homepage from './pages/homepage/hompage';
-import LoginPage from './pages/login/login-page';
-import RegisterPage from './pages/register/registerpage';
 import Main from './pages/mainpage/main';
 import SurveyPage from './pages/surveypage/survey-page';
 import ResultPage from './pages/resultpage/resultpage';
@@ -21,14 +19,12 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" render = {() => !this.props.user.currentUser ? (<Homepage/>) : (<Redirect to='/surveys'/>)}/>
-          <Route exact path='/login' render = {() => !this.props.user.currentUser ? (<LoginPage/>) : (<Redirect to='/surveys'/>)} />
-          <Route exact path='/register' render = {() => !this.props.user.currentUser ? (<RegisterPage/>) : (<Redirect to='/surveys'/>)} />
+          <Route exact path='/auth' component ={GoogleAuth} />
           <Route exact path = '/surveys' render = {() => this.props.user.currentUser ? (<Main/>) : (<Redirect to='/login'/>)} />
           <Route exact path = '/surveys/addnew' render = {() => this.props.user.currentUser ? (<NewSurveyPage/>) : (<Redirect to='/login'/>)} />
           <Route exact path = '/survey/:id' render = {() => this.props.user.currentUser ? (<SurveyPage/>) : (<Redirect to='/login'/>)} />
           <Route exact path = '/survey/:id/results' render = {() => this.props.user.currentUser ? (<ResultPage />) : (<Redirect to='/login'/>)} />
           <Route exact path = '/error' component = {ErrorPage} />
-          <Route exact path='/googleauth' component ={GoogleAuth} />
           <Route component={ErrorPage}/>
         </Switch>
       </div>
