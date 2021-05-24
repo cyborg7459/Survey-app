abeba4d9ff30e647220432c21b399f6f1dd05011
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-25 01:32:51 +0530

    complete the log out functionality with database update

--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -1,27 +1,80 @@
-import './header-style.scss';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import './header-style.scss';
+import {firestore} from '../../firebase/firebase.utils';
 import {logUserOut} from '../../redux/users/user-actions';
+import Loader from '../loader/loader.component';
 
-const Header = (props) => {
-    const users = props.users;
-    return (
-        <div id="header-container">
-            <div id="header">
-                <span onClick={() => {
-                    props.history.push('/');
-                }} style={{cursor : 'pointer'}} className='size20'>What's your opinion ?</span>
+class Header extends React.Component {
+
+    state = {
+        isLoading : false
+    }
+
+    componentDidMount() {
+        this.setState({
+            isLoading : false
+        })
+    }
+    
+    logOutUser = async () => {
+        this.setState({
+            isLoading : true
+        })
+        const userRef = firestore.collection('users').doc(this.props.users.currentUser.id);
+        await userRef.update({isLoggedIn : false});
+        this.props.logOut();
+        this.setState({
+            isLoading : false
+        })
+    }
+
+    render() {
+        return (
+            <div id="header-container">
                 {
-                    users.currentUser ? 
-                    <span className='size13'> Signed in as <strong>{users.currentUser.name.split(' ')[0]}</strong> &nbsp; &nbsp; <span onClick={() => props.logOut()} style={{cursor: 'pointer'}}>Sign out</span> </span> 
-                    : 
-                    <span className='size13' style={{cursor : 'pointer'}} onClick={() => {props.history.push('/login')}}>Login</span>
+                    this.state.isLoading ? <Loader text = "Signing you out" /> : null
                 }
+                <div id="header">
+                    <span onClick={() => {
+                        this.props.history.push('/');
+                    }} style={{cursor : 'pointer'}} className='size20'>What's your opinion ?</span>
+                    {
+                        this.props.users.currentUser ? 
+                        <span className='size13'> Signed in as <strong>{this.props.users.currentUser.name.split(' ')[0]}</strong> &nbsp; &nbsp; <span onClick={this.logOutUser} style={{cursor: 'pointer'}}>Sign out</span> </span> 
+                        : 
+                        <span className='size13' style={{cursor : 'pointer'}} onClick={() => {this.props.history.push('/login')}}>Login</span>
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+// const Header = (props) => {
+
+    
+
+    
+
+//     return (
+//         <div id="header-container">
+//             <div id="header">
+//                 <span onClick={() => {
+//                     props.history.push('/');
+//                 }} style={{cursor : 'pointer'}} className='size20'>What's your opinion ?</span>
+//                 {
+//                     users.currentUser ? 
+//                     <span className='size13'> Signed in as <strong>{users.currentUser.name.split(' ')[0]}</strong> &nbsp; &nbsp; <span onClick={() => props.logOut()} style={{cursor: 'pointer'}}>Sign out</span> </span> 
+//                     : 
+//                     <span className='size13' style={{cursor : 'pointer'}} onClick={() => {props.history.push('/login')}}>Login</span>
+//                 }
+//             </div>
+//         </div>
+//     )
+// }
 
 const mapStateToProps = state => ({
     users : state.users
