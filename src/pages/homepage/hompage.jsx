cdc4e7bbdb7b6d6a4bc9a37e054135d57bd9e5b8
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-25 02:14:58 +0530

    minor styling changes

--- src/pages/homepage/hompage.jsx
+++ src/pages/homepage/hompage.jsx
@@ -8,7 +8,7 @@ const Homepage = (...props) => {
         <div id="homepage-container" className='full-screen'>
             <div className="custom-shape-divider-bottom-1620380295" >
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
                 </svg>
             </div>
             <div id="homepage-inner">
@@ -23,7 +23,7 @@ const Homepage = (...props) => {
                 </p>
                 <button onClick={() => {
                     props[0].history.push('/auth');
-                }} class="btn" id="get-started-btn">
+                }} className="btn" id="get-started-btn">
                     Get started
                 </button>
             </div>
