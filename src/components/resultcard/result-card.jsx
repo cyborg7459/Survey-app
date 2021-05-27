1b005a1edc10f8f2ee5c439552fa3c755bb6b488
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-27 20:02:35 +0530

    responsive changes to the results page

--- src/components/resultcard/result-card.jsx
+++ src/components/resultcard/result-card.jsx
@@ -29,7 +29,7 @@ class ResultCard extends React.Component {
                 backgroundColor: colorArray,
                 borderWidth: 0,
                 hoverBackgroundColor : 'black',
-                hoverOffset : 12,
+                hoverOffset : 5,
                 offset : 0,
                 rotation : 0,
                 animation : {
@@ -43,7 +43,7 @@ class ResultCard extends React.Component {
         return (
             <div className="result-card">
                 <div className="card-inner">
-                    <h1 className='size18 mb-4 text-center'>{this.props.data.title}</h1>
+                    <h1 className='size17 mb-4 text-center'>{this.props.data.title}</h1>
                     <div className="chart-container">
                         <Doughnut options={{
                             animations : {
